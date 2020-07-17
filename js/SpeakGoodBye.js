7046babd04e14570c8a036e1010e82363328246b
jennabrach <67030650+jennabrach@users.noreply.github.com>
2020-07-17 14:00:13 -0400

    module 4 project

---
+++ js/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+
+(function (window) {
+    var byeSpeaker = {};
+    var speakword = "Good Bye";
+    byeSpeaker.speak = function (names) {
+        console.log(speakword + " " + names);
+}
+    window.byeSpeaker = byeSpeaker;
+})
+
+(window);
+
---
+++ js/SpeakHello.js
@@ -0,0 +1,12 @@
+
+(function (window) {
+    var helloSpeaker = {};
+    var speakword = "Hello";
+    helloSpeaker.speak = function (names) {
+        console.log(speakword + " " + names);
+}
+    window.helloSpeaker = helloSpeaker;
+})
+
+(window);
+
